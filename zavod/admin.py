7d206eca969e512e0929958cd6d8b83e7e2a04af
01knappe <36029090+01knappe@users.noreply.github.com>
2020-04-30 17:44:35 +0200

    0.1

---
+++ zavod/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(klub)
+admin.site.register(zavodnik)
+admin.site.register(zavod)
+admin.site.register(kategorie)
+admin.site.register(partneri)
